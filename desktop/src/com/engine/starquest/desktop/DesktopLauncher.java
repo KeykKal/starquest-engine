1c9062206fb1e712f1c25af6189df9b10dc39b9d
KeykKal <73116439+KeykKal@users.noreply.github.com>
2022-02-01 19:35:40 +0100

    start

---
+++ desktop/src/com/engine/starquest/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.engine.starquest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.engine.starquest.Starquest-engine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Starquest-engine(), config);
+	}
+}
